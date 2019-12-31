a4ddae82250f693f2308d3dfc867efae00e2284e
Victor Briceño <40246864+Flaresstorm@users.noreply.github.com>
2019-12-31 10:09:18 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 
-
+//Desde aqui se ejecuta el component principal App al index.js
 ReactDOM.render(<App />, document.getElementById('root'));
 
